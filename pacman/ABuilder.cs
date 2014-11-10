9438ceb16cfaecacd19c83234053fdcf0a202554
Pieter Van Molle <van.molle.pieter@gmail.com>
2014-11-10 14:50:22 +0100

    Add builder

---
+++ pacman/ABuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    abstract class ABuilder
+    {
+        internal IGameObject[][] objects;
+        internal int indexX, indexY;
+
+        public ABuilder()
+        {
+            indexX = 0;
+            indexY = 0;
+        }
+
+        public abstract void BuildPart(string type);
+
+        public IGameObject[][] GetWorld()
+        {
+            return objects;
+        }
+    }
+}
---
+++ pacman/Builder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    public class Builder : ABuilder
+    {
+        public override void AddGameObject(string type)
+        {
+            IGameObject gameObject;
+
+            switch (type)
+            {
+                case "pacman":
+                    gameObject = new Pacman(objects);
+                    break;
+                default:
+                    throw new Exception("Invalid part type");
+            }
+
+            objects[indexY][indexX] = gameObject;
+            indexX++;
+            if (indexX >= objects[indexY].Length)
+            {
+                indexY++;
+                indexX = 0;
+            }
+        }
+    }
+}
