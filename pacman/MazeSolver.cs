17519d481d3ea3a5b5328330011e935e7d668554
Pieter Van Molle <van.molle.pieter@gmail.com>
2014-11-18 23:51:09 +0100

    Create dedicated file for pathfinding

---
+++ pacman/MazeSolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    public class MazeSolver
+    {
+        public static Vector2D SolveForDirection(Field field, Vector2D from, Vector2D to)
+        {
+            Queue<Vector2D> queue  = new Queue<Vector2D>();
+
+            AGameObject[,] gameObjects = field.GameObjects;
+            int height = gameObjects.GetLength(0);
+            int width = gameObjects.GetLength(1);
+            int[,] weights = new int[height, width];
+
+            queue.Enqueue(to);
+
+            while (queue.Count > 0)
+            {
+                Vector2D fetched = queue.Dequeue();
+                int x, y;
+                foreach (Vector2D d in AMoveable.Directions)
+                {
+                    y = fetched.Y + d.Y;
+                    x = fetched.X + d.X;
+                    if (field.Contains(x, y))
+                    {
+                        AGameObject g = gameObjects[y, x];
+                        Vector2D toAdd = new Vector2D(x, y);
+                        if (g is Wall)
+                        {
+                            weights[y, x] = int.MaxValue;
+                        }
+                        else if (!(x == to.X && y == to.Y) && !queue.Contains(toAdd) && weights[y, x] == 0)
+                        {
+                            weights[y, x] = weights[fetched.Y, fetched.X] + 1;
+                            queue.Enqueue(toAdd);
+                        }
+                    }
+                }
+            }
+
+            int value = int.MaxValue;
+            Vector2D direction = new Vector2D(0, 0);
+
+            foreach (Vector2D d in AMoveable.Directions)
+            {
+                int y = from.Y + d.Y;
+                int x = from.X + d.X;
+                if (field.Contains(x, y) && weights[y, x] < value && !(gameObjects[y, x] is Enemy))
+                {
+                    value = weights[y, x];
+                    direction = d;
+                }
+            }
+
+            return direction;
+        }
+    }
+}
--- pacman/StrategyBlinky.cs
+++ pacman/StrategyBlinky.cs
@@ -12,61 +12,10 @@ namespace Pacman
     /// </summary>
     class StrategyBlinky : IStrategy
     {
-        Queue<Vector2D> queue;
-        int[,] weights;
 
         public void Loop(AMoveable gameObject)
         {
-            queue = new Queue<Vector2D>();
-
-            Field gameField = gameObject.Field;
-            AGameObject[,] gameObjects = gameField.GameObjects;
-            int height = gameObjects.GetLength(0);
-            int width = gameObjects.GetLength(1);
-            weights = new int[height, width];
-
-            queue.Enqueue(new Vector2D(gameField.Pacman.Location.X, gameField.Pacman.Location.Y));
-
-            while (queue.Count > 0)
-            {
-                Vector2D fetched = queue.Dequeue();
-                int x, y;
-                foreach (Vector2D d in AMoveable.Directions)
-                {
-                    y = fetched.Y + d.Y;
-                    x = fetched.X + d.X;
-                    if (gameField.Contains(x, y))
-                    {
-                        AGameObject g = gameObjects[y, x];
-                        Vector2D toAdd = new Vector2D(x, y);
-                        if (g is Wall)
-                        {
-                            weights[y, x] = int.MaxValue;
-                        }
-                        else if (!(g is Pacman) && !queue.Contains(toAdd) && weights[y, x] == 0)
-                        {
-                            weights[y, x] = weights[fetched.Y, fetched.X] + 1;
-                            queue.Enqueue(toAdd);
-                        }
-                    }
-                }
-            }
-
-            int value = int.MaxValue;
-            Vector2D direction = new Vector2D(0, 0);
-
-            foreach (Vector2D d in AMoveable.Directions)
-            {
-                int y = gameObject.Location.Y + d.Y;
-                int x = gameObject.Location.X + d.X;
-                if (gameField.Contains(x, y) && weights[y, x] < value && !(gameObjects[y, x] is Enemy))
-                {
-                    value = weights[y, x];
-                    direction = d;
-                }
-            }
-
-            gameObject.Direction = direction;
+            gameObject.Direction = MazeSolver.SolveForDirection(gameObject.Field, gameObject.Location, gameObject.Field.Pacman.Location);
             gameObject.Speed = .2;
         }
     }
